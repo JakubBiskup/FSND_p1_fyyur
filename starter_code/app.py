879c90c26fd6867bdad02b890dcd422ffaab304a
Jakub Biskup <biskup.jakub@gmail.com>
2019-12-26 19:34:22 +0100

    Modify search_venues to search the database

--- starter_code/app.py
+++ starter_code/app.py
@@ -124,13 +124,14 @@ def search_venues():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+  search_entry=request.form.get('search_term', '')
+  data=Venue.query.filter(Venue.name.ilike(f'%{search_entry}%'))
+  count=data.count()
+
+
   response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
+    "count": count,
+    "data": data
   }
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
