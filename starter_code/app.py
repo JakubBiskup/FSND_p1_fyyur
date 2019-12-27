91c0d928d2d88d5fd60839d1e956aa1284b064f1
Jakub Biskup <biskup.jakub@gmail.com>
2019-12-27 19:38:30 +0100

    Modify /shows to display shows from the database

--- starter_code/app.py
+++ starter_code/app.py
@@ -64,7 +64,7 @@ class Show(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   artist_id=db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
   venue_id=db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-  date=db.Column(db.DateTime, nullable=False)
+  start_time=db.Column(db.String(50), nullable=False)
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
@@ -472,42 +472,13 @@ def shows():
   # displays list of shows at /shows
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "venue_id": 1,
-    "venue_name": "The Musical Hop",
-    "artist_id": 4,
-    "artist_name": "Guns N Petals",
-    "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-    "start_time": "2019-05-21T21:30:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 5,
-    "artist_name": "Matt Quevedo",
-    "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-    "start_time": "2019-06-15T23:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-01T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-08T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-15T20:00:00.000Z"
-  }]
+
+  data=Show.query.all() 
+ 
+  
+ 
+
+
   return render_template('pages/shows.html', shows=data)
 
 @app.route('/shows/create')
@@ -522,7 +493,7 @@ def create_show_submission():
     artist_id=request.form.get('artist_id')
     venue_id=request.form.get('venue_id')
     date=request.form.get('start_time')
-    new_show=Show(artist_id=artist_id,venue_id=venue_id,date=date)
+    new_show=Show(artist_id=artist_id,venue_id=venue_id,start_time=date)
     db.session.add(new_show)
     db.session.commit()
 
