2b54b2a018b023037dc16bd77053df8ed85a0848
Jakub Biskup <biskup.jakub@gmail.com>
2020-01-01 17:36:57 +0100

    Add delete button on /venues/<venue_id>

--- starter_code/app.py
+++ starter_code/app.py
@@ -187,12 +187,22 @@ def create_venue_submission():
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
+  try:
+    Show.query.filter_by(venue_id=venue_id).delete()
+    Venue.query.filter_by(id=venue_id).delete()
+    db.session.commit()
+    flash('Venue was successfully deleted!')
+  except:
+    db.session.rollback()
+    flash('An error occurred. Venue ' + Venue.query.filter_by(id=venue_id).name + ' could not be deleted.') 
+  finally:
+    db.session.close()
   # TODO: Complete this endpoint for taking a venue_id, and using
   # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
 
   # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
   # clicking that button delete it from the db then redirect the user to the homepage
-  return None
+    return render_template('pages/home.html')
 
 #  Artists
 #  ----------------------------------------------------------------
